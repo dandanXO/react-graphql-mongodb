aeac91536a8303639c1332b82942da4c8a97fc9d
Dan <pigpig850130@gmail.com>
2019-08-03 22:34:12 +0800

    add booking feature add pure css loading animation

--- frontend/src/components/Events/EventLis/EventList.js
+++ frontend/src/components/Events/EventLis/EventList.js
@@ -5,12 +5,16 @@ import './EventList.css';
 const eventList = props => {
   const events = props.events.map(event => {
     return (
-      <EventItem key={event._id} 
-      eventId={event._id}
-       title={event.title}
-        userId={props.authUserId} 
+      <EventItem
+        key={event._id}
+        eventId={event._id}
+        title={event.title}
+        price={event.price}
+        date={event.date}
+        userId={props.authUserId}
         creatorId={event.creator._id}
-        />
+        onDetail={props.onViewDetail}
+      />
     )
   })
   return <ul className="event__list">{events}</ul>
